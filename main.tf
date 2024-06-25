907b39dade9cd1836cf0caaae8e22ec74d675cba
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-06-26 04:04:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-01b799c439fd5516a"
+  ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
 
   tags = {
